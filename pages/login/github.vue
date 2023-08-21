4a813903af9ee59e4d7e337233a98a47cb89040c
b01010110 <b01010110b@gmail.com>
2023-08-21 16:56:53 +0700

    auth has been added

---
+++ pages/login/github.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Waiting for github authorization ...</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+const { signIn } = useAuth();
+
+onMounted(async () => {
+  await signIn({ code: route.query.code }, { redirect: false });
+  await navigateTo("/");
+});
+</script>
+
+<style scoped lang="scss"></style>
