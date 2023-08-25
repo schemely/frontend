aef0ed78ce47212f3af39770e4fecd19e2761f66
b01010110 <b01010110b@gmail.com>
2023-08-25 15:57:22 +0700

    profile button has been added

---
+++ entities/profile/ui/ProfileButton.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="profile-button">
+    <div class="profile-button__avatar-wrap">
+      <VImg v-if="avatar" class="profile-button__avatar" :src="avatar" />
+      <NuxtIcon v-else class="profile-button__default-avatar" name="account-circle-outline" />
+    </div>
+    <div class="profile-button__login">{{ login }}</div>
+    <NuxtIcon class="profile-button__chevron" name="chevron-down" />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  login: string
+  avatar?: string
+}>()
+</script>
+
+<style scoped lang="scss">
+.profile-button {
+  display: inline-flex;
+  align-items: center;
+  padding: 4px;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: $hover;
+    cursor: pointer;
+  }
+
+  &__avatar-wrap {
+    margin-right: 4px;
+    width: 24px;
+    height: 24px;
+  }
+
+  &__avatar {
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+  }
+
+  &__default-avatar {
+    font-size: 0;
+
+    & :deep(svg) {
+      width: 24px;
+      height: 24px;
+      color: $text__text;
+    }
+  }
+
+  &__login {
+    margin-right: 4px;
+    @include default;
+    line-height: 150%;
+    color: $text__text;
+  }
+
+  &__chevron {
+    font-size: 0;
+
+    & :deep(svg) {
+      width: 16px;
+      height: 16px;
+      color: $text__text;
+    }
+  }
+}
+</style>
---
+++ widgets/profile/ui/ProfileButton.vue
@@ -0,0 +1,10 @@
+<template>
+  <EntityProfileButton :login="login" :avatar="avatar" />
+</template>
+
+<script setup lang="ts">
+const login = ref('b01010110')
+const avatar = ref('')
+</script>
+
+<style scoped lang="scss"></style>
