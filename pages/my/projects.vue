c4b3e5a9167cb0a32d9c6e9817295388f78a7559
b01010110 <b01010110b@gmail.com>
2023-08-26 13:52:54 +0700

    my project test page has been added

---
+++ pages/my/projects.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NuxtLink to="/">Main page</NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'projects',
+  middleware: 'auth',
+})
+</script>
+
+<style scoped lang="scss"></style>
