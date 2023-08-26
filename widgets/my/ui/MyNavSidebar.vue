158c52e31c657aea69aaeef932a5d746c11249f6
b01010110 <b01010110b@gmail.com>
2023-08-26 13:53:21 +0700

    my nav sidebar widget has been added

---
+++ widgets/my/ui/MyNavSidebar.vue
@@ -0,0 +1,24 @@
+<template>
+  <VNavigationDrawer class="my-nav-sidebar" width="250" permanent>
+    <div class="my-nav-sidebar__profile-notification">
+      <WidgetProfileButton />
+      <WidgetNotificationButton />
+    </div>
+  </VNavigationDrawer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.my-nav-sidebar {
+  border-color: $border;
+
+  &__profile-notification {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 4px;
+    padding: 8px;
+  }
+}
+</style>
