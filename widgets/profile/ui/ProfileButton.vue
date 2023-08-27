d75c30e9257e5a986f4f4b2b234392fa9e328843
b01010110 <b01010110b@gmail.com>
2023-08-27 21:07:50 +0700

    profile dropdown menu has been added to profile button

--- widgets/profile/ui/ProfileButton.vue
+++ widgets/profile/ui/ProfileButton.vue
@@ -1,10 +1,36 @@
 <template>
-  <EntityProfileButton :login="login" :avatar="avatar" />
+  <VMenu :close-on-content-click="false">
+    <template #activator="{ props }">
+      <EntityProfileButton v-bind="props" :login="profileMenu.login" :avatar="profileMenu.avatar" />
+    </template>
+    <EntityProfileDropdownMenu v-bind="profileMenu" :menu="menu" />
+  </VMenu>
 </template>
 
 <script setup lang="ts">
-const login = ref('b01010110')
-const avatar = ref('')
+import type { MenuItem } from '@/shared/interfaces/Menu'
+
+const profileMenu = reactive({
+  login: 'b01010110',
+  avatar: '',
+  email: 'b01010110b@gmail.com',
+})
+
+const menu: MenuItem[] = [
+  {
+    text: 'Theme',
+    icon: 'theme-light-dark',
+    submenu: [{ text: 'Light' }, { text: 'Dark' }, { text: 'System theme', isChecked: true }],
+  },
+  {
+    text: 'Settings',
+    icon: 'tune-vertical-variant',
+  },
+  {
+    text: 'Log out',
+    icon: 'logout',
+  },
+]
 </script>
 
 <style scoped lang="scss"></style>
