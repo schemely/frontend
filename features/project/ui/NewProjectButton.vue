5ed57607506caf435fc1c145600624123ea44999
b01010110 <b01010110b@gmail.com>
2023-09-07 18:03:12 +0700

    redirect link has been added into new project button feature

--- features/project/ui/NewProjectButton.vue
+++ features/project/ui/NewProjectButton.vue
@@ -1,7 +1,15 @@
 <template>
-  <UIButton nuxt-icon="file-outline" variation="secondary">Add a new project</UIButton>
+  <UIButton nuxt-icon="file-outline" variation="secondary" @click="projectRedirect">
+    Add a new project
+  </UIButton>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const router = useRouter()
+
+function projectRedirect() {
+  router.push('/my/new-project')
+}
+</script>
 
 <style scoped lang="scss"></style>
