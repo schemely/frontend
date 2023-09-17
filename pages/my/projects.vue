65508d7f92368fab1faedaaaf89b7e16e7632e28
b01010110 <b01010110b@gmail.com>
2023-09-17 22:50:35 +0700

    projects page has been added

--- pages/my/projects.vue
+++ pages/my/projects.vue
@@ -1,17 +1,32 @@
 <template>
-  <div>
-    <NuxtLink to="/">Main page</NuxtLink>
+  <div class="page-my-projects">
+    <div class="page-my-projects__empty-block">
+      <WidgetProjectEmptyBlock variant="my-projects" />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { useMyHeaderStore } from '@/entities/my/stores/header'
+
 definePageMeta({
   layout: 'projects',
   middleware: 'auth',
 })
 
-const headerTitle = useMyHeaderTitle()
-headerTitle.value = 'My projects'
+const { setTitle } = useMyHeaderStore()
+
+onMounted(() => {
+  setTitle('My projects')
+})
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.page-my-projects {
+  &__empty-block {
+    display: flex;
+    justify-content: center;
+    margin-top: 48px;
+  }
+}
+</style>
